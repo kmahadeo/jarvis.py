5959745ba8b59c4a584a4ca6ab8ef71cb68f17dd
kmahadeo <71351480+kmahadeo@users.noreply.github.com>
2021-03-02 14:03:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('Listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'jarvis' in command:
+                command = command.replace('Jarvis', '')
+                talk(command)
+                return command
+    except:
+        pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if 0 <= hour < 12:
+        pyttsx3.speak("Good Morning! How can I help you?")
+
+    elif 12 <= hour < 18:
+        pyttsx3.speak("Good Afternoon! What can I do for you?")
+
+    else:
+        pyttsx3.speak("Good Evening! How may I assist you?")
+
+
+def run_jarvis():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('Playing' + song)
+        pywhatkit.playonyt(song)
+        print(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        print(time)
+        talk('The current time is ' + time)
+    elif 'search' in command:
+        search1 = command.replace('search', '')
+        pywhatkit.search(command)
+        google = search1.summary(search1, 2)
+        talk(google)
+        print(google)
+    elif 'Who' or 'What' or 'Where' in command:
+        person = command.replace('who' or 'what' or 'where', '')
+        info = wikipedia.summary(person, 2)
+        talk(info)
+        print(info)
+    elif 'funny' or 'joke' or 'sarcastic' or 'silly' or 'bored' in command:
+        talk(pyjokes.get_joke())
+        print(pyjokes.get_joke())
+    else:
+        talk('Could you please repeat that?')
+
+
+while True:
+    wishme()
+    run_jarvis()
